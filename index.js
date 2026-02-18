34ae806bd229f50a0facdd34bff5ccbb7fa921dd
Eoghan Mellott <eoghanm2013@gmail.com>
2026-02-18 13:55:59 +0000

    Remove all dependencies - clean state for testing

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const _ = require('lodash');
-
-// Simple test file using vulnerable lodash version
+// Simple test file
 console.log('Testing Code Security team tag behavior');
-console.log('Lodash version:', _.VERSION);
+console.log('No dependencies - clean state');
 
